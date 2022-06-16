745aa1166b4767cfbbe239deae2affa3dd4f3290
Andrey <AndreyHlopkov@gmaile.com>
2022-06-16 10:42:54 +0300

    two ways register admin models

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
 
-admin.site.register(Author)
-admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+
+
+# admin.site.register(BookInstance)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
