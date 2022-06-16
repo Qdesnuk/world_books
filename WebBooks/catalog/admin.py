ffe8448750af036bb17a12280727c91e2f460ee5
Andrey <AndreyHlopkov@gmaile.com>
2022-06-16 11:12:54 +0300

    change display Author fields

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -12,7 +12,8 @@ admin.site.register(Status)
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name')
+    fields = ('first_name', 'last_name', ('date_of_birth', 'date_of_death'))
 
 
 admin.site.register(Author, AuthorAdmin)
