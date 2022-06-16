c6974e9e45033413234556ff79862a4ef37110e0
Andrey <AndreyHlopkov@gmaile.com>
2022-06-17 02:11:32 +0300

    create base and index templates

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse('Главная страница сайта Мир книг!')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_avaliable = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+    return render(request, 'index.html', context={'num_books': num_books,
+                                                  'num_instances': num_instances,
+                                                  'num_instances_avaliable': num_instances_avaliable,
+                                                  'num_authors': num_authors})
